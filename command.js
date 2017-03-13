e32fbff4ec147608435de1a73381e3e418a37f76
Alex Drummond <a.d.drummond@gmail.com>
2017-03-13 10:42:19 +0000

    Nearly complete.

--- command.js
+++ command.js
@@ -75,6 +75,28 @@ function randomTree(nPhrases, letters) {
     return treelets[0];
 }
 
+function randomPathPair(tree) {
+    let paths = [ ];
+
+    (function rec (tree, path) {
+        paths.push([tree, path]);
+        for (var i = 0; i < tree.children.length; ++i) {
+            rec(tree.children[i], path + i)
+        }
+    })(tree, '');
+
+    var i1 = parseInt(Math.round(Math.random() * (paths.length-1)));
+    var i2 = parseInt(Math.round(Math.random() * (paths.length-1)));
+    if (i1 == i2) {
+        if (i1 > 0)
+            --i1;
+        else
+            ++i1;
+    }
+
+    return [paths[i1], paths[i2]];
+}
+
 function treeToSimpleWidthTree(tree, measureWidth) {
     if (tree.children.length == 0) {
         return {
@@ -204,29 +226,95 @@ function layoutTree(tree, measureWidth, levelHeight, xOffset, yOffset) {
     return tree;
 }
 
-function renderTree(ctx, tree, fontSize, levelHeight, hpad) {
+function renderTree(ctx, tree, fontSize, levelHeight, hpad, highlights) {
     ctx.font = fontSize + "px Helvetica";
+
+    var yMargin = fontSize * 2;
 
     tree = layoutTree(
         tree,
         function (t) { return ctx.measureText(hpad + t.label + hpad).width },
         levelHeight,
-        0,
-        0
+        fontSize * 2, // Give a bit of a left margin
+        yMargin
     );
 
     ctx.font = fontSize + "px Helvetica";
 
-    (function rec(tree) {
+    var treeHeight = 0;
+    var treeWidth = 0;
+    (function rec(tree, path, height) {
+        ctx.fillStyle = "#000000";
         ctx.fillText(hpad + tree.label + hpad, tree.x - tree.labelWidth/2, tree.y + fontSize);
+
+        if (tree.y > treeHeight)
+            treeHeight = tree.y;
+        if (tree.x + tree.labelWidth/2 > treeWidth)
+            treeWidth = tree.x + tree.labelWidth/2;
+
         for (var i = 0; i < tree.children.length; ++i) {
+            ctx.beginPath();
             ctx.moveTo(tree.x, tree.y + fontSize * 1.25);
             ctx.lineTo(tree.children[i].x,
                        tree.children[i].y - fontSize*0.05);
+            ctx.strokeStyle = "#000000";
             ctx.stroke();
-            rec(tree.children[i]);
+            ctx.closePath();
+            rec(tree.children[i], path + i);
         }
-    })(tree);
+
+        if (highlights[path]) {
+            ctx.beginPath();
+            ctx.arc(tree.x, tree.y + fontSize*0.5, fontSize*1.5, 0, 2*Math.PI);
+            if (highlights[path].commander) {
+                ctx.strokeStyle = "#00FF00";
+            }
+            else if (highlights[path].commanded) {
+                ctx.strokeStyle = "#FF0000";
+            }
+            ctx.stroke();
+            ctx.closePath();
+        }
+    })(tree, [ ]);
+
+    treeHeight += levelHeight;
+    return [treeWidth, treeHeight];
+}
+
+function poseQuestion(canvas, ctx, qdiv) {
+    // Clear the canvas.
+    ctx.save();
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.restore();
+
+    var tree = randomTree(10, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    var pathPair = randomPathPair(tree);
+    var highlights = { };
+    highlights[pathPair[0][1]] = { commander: true };
+    highlights[pathPair[1][1]] = { commanded: true };
+    var wh = renderTree(ctx, tree, 12, 30, "   ", highlights);
+
+    while (qdiv.hasChildNodes()) {
+        qdiv.removeChild(qdiv.lastChild);
+    }
+
+    qdiv.style.position = 'relative';
+    qdiv.style.top = wh[1] + 'px';
+    qdiv.style.width = wh[0] + 'px';
+    qdiv.style.textAlign = 'center';
+
+    var q = document.createElement("div");
+    q.style.margin = "0";
+    q.style.padding = "0";
+    q.style.position = "relative";
+    q.style.top = "2em";
+    q.style.textAlign = 'center';
+
+    q.appendChild(
+        document.createTextNode("Does " + pathPair[0][0].label + " c-command " + pathPair[1][0].label + "?")
+    );
+    qdiv.appendChild(q);
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
@@ -244,6 +332,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var ctx = canvas.getContext("2d");
     ctx.scale(SCALE, SCALE);
 
-    let tree = randomTree(10, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-    renderTree(ctx, tree, 12, 30, "   ");
+    poseQuestion(canvas, ctx, document.getElementById("question"));
 });
